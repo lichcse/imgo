afcbd8f0fd4ccb7b66b1631f5e7fa5aecc6a478d
Lich Truong <lichtv@sendo.vn>
2020-08-09 16:11:02 +0700

    Update start up script

--- startup/app.go
+++ startup/app.go
@@ -3,64 +3,47 @@ package startup
 import (
 	"imgo/src/routes"
 	"imgo/src/utils"
-	"log"
+
+	"imgo/src/common"
 
 	// mysql driver
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
 
-var (
-	config utils.IMConfig
-)
-
 // InitApp func
 func InitApp(args []string) error {
 
-	err := loadConfig(args)
+	resource := common.NewIMResource(args)
+	config, err := resource.Config()
 	if err != nil {
 		return err
 	}
 
-	sqlConfig := config.MySQLItem("im")
-	db, err := gorm.Open("mysql", sqlConfig.URL)
-	defer db.Close()
+	mySQL, err := resource.MySQLConn()
 	if err != nil {
 		return err
 	}
+	if mySQL != nil {
+		defer mySQL.Close()
+	}
 
-	rabbitMQConfig := config.RabbitMQ()
-	rab := utils.NewRabbitMQ(rabbitMQConfig.URL)
-	rabbitMQConn, err := rab.Connect()
-	defer rabbitMQConn.Close()
+	rabbitMQConn, err := resource.RabbiMQConn()
 	if err != nil {
 		return err
 	}
-
-	go rab.HealthCheck(func(mess string) {
-		log.Printf("%s", mess)
-	})
+	if rabbitMQConn != nil {
+		defer rabbitMQConn.Close()
+	}
 
 	if len(args) <= 1 {
-		return setRoute(args, db)
+		return setRoute(config, mySQL)
 	}
 	return nil
-}
-
-// loadConfig func
-func loadConfig(args []string) error {
-	config = utils.NewIMConfig()
-	return config.Load(args)
 }
 
 // setRoute func
-func setRoute(args []string, db *gorm.DB) error {
-	config := utils.NewIMConfig()
+func setRoute(config utils.IMConfig, db *gorm.DB) error {
 	router := routes.SetupRouter(db)
 	return router.Run(config.GetPort())
-}
-
-func callback(data string) error {
-	// TODO
-	return nil
 }
